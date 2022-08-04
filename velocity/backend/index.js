463937f6ee558de2b81efebbbc7b4bd46ce50054
Dinesh-Talwadker <talwadkerdinesh@gmail.com>
2022-08-04 20:58:01 +0530

    ⚙ bare setup done

---
+++ velocity/backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(path.join(__dirname, "../frontend/")));
+
+// routes
+const MainRouter = require("../backend/routes/main")
+app.use("/", MainRouter)
+const GameRouter = require("../backend/routes/game")
+app.use("/game", GameRouter)
+
+app.listen(8080, ()=> console.log("server started"))
---
+++ velocity/backend/routes/main.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const Router = express()
+
+Router.get("/", (req,res)=>{
+    res.render("../frontend/")
+})
+
+Router.get("/info", (req,res)=>{
+    res.json({status:"400"})    
+    // status check and response in json
+})
+
+module.exports = Router
